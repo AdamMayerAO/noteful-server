f615224e594f3521bd410199dfafa09ca29c1d69
Adam Mayer <r.adammayer@gmail.com>
2021-04-12 19:45:01 -0400

    Yo

--- postgrator-config.js
+++ postgrator-config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg", 
-    "connectionString": process.env.DATABASE_URL
-    // ssl: {
-    //     rejectUnauthorized: false
-    //   }
+    "connectionString": process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+      }
 }
