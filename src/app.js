da00b2e53eef58f7c61b4b787cf15cea5f36b25c
Adam Mayer <r.adammayer@gmail.com>
2021-04-19 22:38:42 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -15,6 +15,8 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
+app.use(cors())
+
 
 app.use('/folders', foldersRouter)
 app.use('/notes', notesRouter)
@@ -32,6 +34,5 @@ app.use(function errorHandler(error, req, res, next) {
        }
        res.status(500).json(response)
      })
-app.use(cors())
 
 module.exports = app
